de14709b0290faf81be82c3a179995fe6e3445ab
yousafsabir <yousafsabir430516@gmail.com>
2022-07-02 06:06:59 +0500

    added a toggle in nav items

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
+  toggle = true;
+  @Output() toggleEmit = new EventEmitter();
+
+  onRecipe() {
+    this.toggle = true;
+    this.toggleEmit.emit(this.toggle);
+  }
+  onShopping() {
+    this.toggle = false;
+    this.toggleEmit.emit(this.toggle);
+  }
+
   manage = false;
 
   constructor() {}
